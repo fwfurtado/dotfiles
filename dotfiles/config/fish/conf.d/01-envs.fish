d531e8981a47cc9be68bc24ba26d87c66b04351e
Fernando <fwfurtado@gmail.com>
2023-04-29 18:13:51 -0300

    add fish helpers

---
+++ dotfiles/config/fish/conf.d/01-envs.fish
@@ -0,0 +1,5 @@
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.local/bin
+
+
+export BUF_TOKEN="<TOKEN>"
---
+++ dotfiles/config/fish/conf.d/03-alias-base.fish
@@ -0,0 +1,31 @@
+alias pbcopy="xsel --clipboard --input"
+alias pbpaste="xsel --clipboard --output"
+
+function md 
+
+	argparse 'k/keep' -- $argv 
+	or return
+
+	mkdir -p $argv
+
+	if set -ql _flag_k 
+		return $status
+	end 
+
+	if test $status -ne 0 
+		return $stauts
+	end
+	
+	cd $argv
+end
+
+
+function bang_bang
+	echo $history[1]
+end
+
+abbr --add !! --position anywhere --function bang_bang
+
+alias please='sudo' 
+
+
