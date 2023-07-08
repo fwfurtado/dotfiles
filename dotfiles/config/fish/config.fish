ee94c8c3edc0ef33dd9e390611643ff2148ff96e
Fernando <fwfurtado@gmail.com>
2023-07-07 21:25:47 -0300

    update fish configuration

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,3 +1,4 @@
+set fish_greeting
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
@@ -8,3 +9,6 @@ end
 # Wasmer
 export WASMER_DIR="/var/home/anyone/.wasmer"
 [ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"
+
+# Generated for envman. Do not edit.
+test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
