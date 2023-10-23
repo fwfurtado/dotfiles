31a2a36512829b986c40e88d69cb2c6a88a3dc11
Fernando <fwfurtado@gmail.com>
2023-10-23 14:51:15 -0300

    update dotfiles

--- dotfiles/config/fish/conf.d/01-envs.fish
+++ dotfiles/config/fish/conf.d/01-envs.fish
@@ -1,10 +1,16 @@
-fish_add_path $HOME/.cargo/bin
-fish_add_path $HOME/.local/bin
-fish_add_path $HOME/bin
-fish_add_path $HOME/.dotnet/tools
-fish_add_path $HOME/.krew/bin
-
-# export DOCKER_HOST=unix:///run/user/1000/docker.sock # only for rootless docker
+export SYSTEMD_EDITOR=nvim
+export EDITOR=nvim
+export ANDROID_SDK=$HOME/Android/Sdk
+export DOTNET_ROOT=$HOME/.dotnet
 export SYSTEMD_EDITOR=nvim
 export EDITOR=nvim
 
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/bin
+fish_add_path $DOTNET_ROOT
+fish_add_path $DOTNET_ROOT/tools
+fish_add_path $HOME/.krew/bin
+fish_add_path $ANDROID_SDK/platform-tools
+fish_add_path $ANDROID_SDK/tools/bin
+fish_add_path $ANDROID_SDK/tools
--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,1 +1,3 @@
 set fish_greeting
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
