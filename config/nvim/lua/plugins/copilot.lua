40840f08ece958d964c470047e4b6d5bf8097b21
Mimi <fwfurtado@gmail.com>
2025-04-08 12:27:45 -0300

    update nvim configs and add ghostty completions

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,24 +1,14 @@
 return {
-    {
+    "zbirenbaum/copilot-cmp",
+    event = "InsertEnter",
+    config = function () require("copilot_cmp").setup() end,
+    dependencies = {
         "zbirenbaum/copilot.lua",
         cmd = "Copilot",
-        event = "InsertEnter",
         config = function()
             require("copilot").setup({
-                suggestion = {
-                    enabled = true,
-                    auto_trigger = false,
-                    hide_during_completion = false,
-                    debounce = 25,
-                    keymap = {
-                        accept = false,
-                        accept_word = false,
-                        accept_line = "<Tab>",
-                        next = false,
-                        prev = false,
-                        dismiss = false,
-                    },
-                },
+                suggestion = { enabled = false },
+                panel = { enabled = false },
             })
         end,
     },
