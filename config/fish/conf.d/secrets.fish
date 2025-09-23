84aff1cb9d5cb655edb6030489953f4422fc7f95
Mimi <fwfurtado@gmail.com>
2025-09-23 10:47:39 -0300

    fix

--- config/fish/conf.d/secrets.fish
+++ config/fish/conf.d/secrets.fish
@@ -1,2 +1,2 @@
-export GITHUB_PERSONAL_ACCESS_TOKEN={{command_output "op read 'op://Private/GitHub Personal Access Token/token'"}}
+export GITHUB_PERSONAL_ACCESS_TOKEN={{command_output "op read 'op://development/GitHub Personal Access Token/token'"}}
 export GPR_TOKEN=$GITHUB_PERSONAL_ACCESS_TOKEN
