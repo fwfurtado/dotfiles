81a773861f1528bc167d878354f71b6507f39c6f
Mimi <fwfurtado@gmail.com>
2026-02-02 14:45:48 -0300

    feat: add missing completions

--- config/fish/conf.d/secrets.fish
+++ config/fish/conf.d/secrets.fish
@@ -1,2 +1,4 @@
 export GITHUB_PERSONAL_ACCESS_TOKEN={{command_output "op read 'op://development/GitHub Personal Access Token/token'"}}
 export GPR_TOKEN=$GITHUB_PERSONAL_ACCESS_TOKEN
+export CONSUL_HTTP_ADDR={{command_output "op read 'op://homelab/Consul Server/address'"}}
+export CONSUL_HTTP_TOKEN={{command_output "op read 'op://homelab/Consul Server/acl management/secret-id'"}}
