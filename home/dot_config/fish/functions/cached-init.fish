31536daebebb179e99d245dc37236a4804fca20d
Mimi <fwfurtado@gmail.com>
2026-08-25 13:03:11 -0300

    fix: loader

--- home/dot_config/fish/functions/cached-init.fish
+++ home/dot_config/fish/functions/cached-init.fish
@@ -1,7 +1,9 @@
-function cached-init --argument name --description 'sourceia init pré-gerado em ~/.cache/fish/init'
+function cached-init --argument name --description 'sourceia init pré-gerado'
     set -l cache (set -q XDG_CACHE_HOME; and echo $XDG_CACHE_HOME; or echo $HOME/.cache)/fish/init/$name.fish
+    set -l bin (command -v $name)
 
-    if test -f $cache
+    # cache mais novo que o binário, senão regenera
+    if test -f $cache; and test -n "$bin"; and test $cache -nt $bin
         source $cache
         return 0
     end
