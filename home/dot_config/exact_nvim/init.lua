2d86ce3a971d2faa40eb2753b79708498d227ae1
Mimi <fwfurtado@gmail.com>
2026-08-25 15:16:51 -0300

    Update .config/nvim/init.lua

--- home/dot_config/exact_nvim/init.lua
+++ home/dot_config/exact_nvim/init.lua
@@ -54,6 +54,16 @@ vim.schedule(function()
     vim.o.clipboard = 'unnamedplus'
 end)
 
+-- forçar OSC 52 quando o provider nativo não serve
+if vim.env.SSH_TTY then
+    local osc52 = require('vim.ui.clipboard.osc52')
+    vim.g.clipboard = {
+        name = 'OSC 52',
+        copy = { ['+'] = osc52.copy('+'), ['*'] = osc52.copy('*') },
+        paste = { ['+'] = osc52.paste('+'), ['*'] = osc52.paste('*') },
+    }
+end
+
 --------------------------------------------------------------------------------
 -- Plugins
 --------------------------------------------------------------------------------
