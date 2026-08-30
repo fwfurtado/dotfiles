88c4073b33b1d63179a87a793b87d05f76e144c8
Mimi <fwfurtado@gmail.com>
2026-08-30 20:24:19 -0300

    Update .config/nvim/init.lua

--- home/dot_config/exact_nvim/init.lua
+++ home/dot_config/exact_nvim/init.lua
@@ -173,7 +173,9 @@ require('mini.move').setup({
     }
 })
 
-require('mini.animate').setup()
+require('mini.animate').setup({
+    scroll = { enable = false },
+})
 require('mini.indentscope').setup()
 require('mini.trailspace').setup()
 require('mini.statusline').setup()
