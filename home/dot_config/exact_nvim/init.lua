caaa0e36101b3f413f8471a990e5d8448477966b
Mimi <fwfurtado@gmail.com>
2026-09-25 12:03:04 -0300

    Update .config/nvim/init.lua

--- home/dot_config/exact_nvim/init.lua
+++ home/dot_config/exact_nvim/init.lua
@@ -84,6 +84,7 @@ vim.pack.add({
     { src = 'https://github.com/nvim-treesitter/nvim-treesitter',             version = 'main' },
     'https://github.com/nvim-mini/mini.nvim',
     'https://github.com/stevearc/oil.nvim',
+    'https://github.com/FylerOrg/fyler.nvim',
 })
 
 
